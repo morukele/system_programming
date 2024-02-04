941adc1e40564a45af485e04298625837865718b
Oghenemarho Orukele <orukele.dev@gmail.com>
2024-02-04 22:59:46 +0100

    added function calls to the CPU

--- src/chip8/cpu.rs
+++ src/chip8/cpu.rs
@@ -2,6 +2,8 @@ pub struct CPU {
     pub memory: [u8; 0x1000],
     pub program_counter: usize,
     pub registers: [u8; 16],
+    pub stack: [u16; 16],
+    pub stack_pointer: usize,
 }
 
 impl Default for CPU {
@@ -16,6 +18,8 @@ impl CPU {
             registers: [0; 16],
             program_counter: 0,
             memory: [0; 0x1000],
+            stack: [0; 16],
+            stack_pointer: 0,
         }
     }
 
@@ -32,15 +36,21 @@ impl CPU {
             let opcode = self.read_opcode();
             self.program_counter += 2;
 
+            // Chip8 opcode decoding
             let c = ((opcode & 0xF000) >> 12) as u8;
             let x = ((opcode & 0x0F00) >> 8) as u8;
             let y = ((opcode & 0x00F0) >> 4) as u8;
             let d = ((opcode & 0x000F) >> 0) as u8;
 
+            // Memory address of the function
+            let nnn = opcode & 0x0FFF;
+
             match (c, x, y, d) {
                 (0, 0, 0, 0) => {
                     return;
                 }
+                (0, 0, 0xE, 0xE) => self.ret(),
+                (0x2, _, _, _) => self.call(nnn),
                 (0x8, _, _, 0x4) => self.add_xy(x, y),
                 _ => todo!("opcode {:04x}", opcode),
             }
@@ -59,5 +69,28 @@ impl CPU {
         } else {
             self.registers[0xF] = 0;
         }
+    }
+
+    fn call(&mut self, addr: u16) {
+        let sp = self.stack_pointer;
+        let stack = &mut self.stack;
+
+        if sp > stack.len() {
+            panic!("Stack overflow!")
+        }
+
+        stack[sp] = self.program_counter as u16;
+        self.stack_pointer += 1;
+        self.program_counter = addr as usize;
+    }
+
+    fn ret(&mut self) {
+        if self.stack_pointer == 0 {
+            panic!("Stack underflow");
+        }
+
+        self.stack_pointer -= 1;
+        let call_addr = self.stack[self.stack_pointer];
+        self.program_counter = call_addr as usize;
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -5,20 +5,25 @@ fn main() {
 
     cpu.registers[0] = 5;
     cpu.registers[1] = 10;
-    cpu.registers[2] = 10;
-    cpu.registers[3] = 10;
 
     let mem = &mut cpu.memory;
-    mem[0] = 0x80;
-    mem[1] = 0x14;
-    mem[2] = 0x80;
-    mem[3] = 0x24;
-    mem[4] = 0x80;
-    mem[5] = 0x34;
+    mem[0x000] = 0x21;
+    mem[0x001] = 0x00;
+    mem[0x002] = 0x21;
+    mem[0x003] = 0x00;
+    mem[0x004] = 0x00;
+    mem[0x005] = 0x00;
+
+    mem[0x100] = 0x80;
+    mem[0x101] = 0x14;
+    mem[0x102] = 0x80;
+    mem[0x103] = 0x14;
+    mem[0x104] = 0x00;
+    mem[0x105] = 0xEE;
 
     cpu.run();
 
-    assert_eq!(cpu.registers[0], 35);
+    assert_eq!(cpu.registers[0], 45);
 
-    println!("5 + 10 + 10 + 10 = {}", cpu.registers[0]);
+    println!("5 + (10 * 2) + (10 * 2) = {}", cpu.registers[0]);
 }
