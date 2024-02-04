ee490af79d6c8c0d322e14bd5ab0865aa2bc8c71
Oghenemarho Orukele <orukele.dev@gmail.com>
2024-02-04 22:15:53 +0100

    added counter, memory and detecting addition overflow to the cpu

--- src/chip8/cpu.rs
+++ src/chip8/cpu.rs
@@ -1,30 +1,63 @@
 pub struct CPU {
-    pub current_operation: u16,
-    pub registers: [u8; 2],
+    pub memory: [u8; 0x1000],
+    pub program_counter: usize,
+    pub registers: [u8; 16],
+}
+
+impl Default for CPU {
+    fn default() -> Self {
+        Self::new()
+    }
 }
 
 impl CPU {
+    pub fn new() -> Self {
+        Self {
+            registers: [0; 16],
+            program_counter: 0,
+            memory: [0; 0x1000],
+        }
+    }
+
     pub fn read_opcode(&self) -> u16 {
-        self.current_operation
+        let p = self.program_counter;
+        let op_byte1 = self.memory[p] as u16;
+        let op_byte2 = self.memory[p + 1] as u16;
+
+        op_byte1 << 8 | op_byte2
     }
 
     pub fn run(&mut self) {
-        // loop {
-        let opcode = self.read_opcode();
+        loop {
+            let opcode = self.read_opcode();
+            self.program_counter += 2;
 
-        let c = ((opcode & 0xF000) >> 12) as u8;
-        let x = ((opcode & 0x0F00) >> 8) as u8;
-        let y = ((opcode & 0x00F0) >> 4) as u8;
-        let d = ((opcode & 0x000F) >> 12) as u8;
+            let c = ((opcode & 0xF000) >> 12) as u8;
+            let x = ((opcode & 0x0F00) >> 8) as u8;
+            let y = ((opcode & 0x00F0) >> 4) as u8;
+            let d = ((opcode & 0x000F) >> 0) as u8;
 
-        match (c, x, y, d) {
-            (0x8, _, _, 0x4) => self.add_xy(x, y),
-            _ => todo!("opcode {:04x}", opcode),
+            match (c, x, y, d) {
+                (0, 0, 0, 0) => {
+                    return;
+                }
+                (0x8, _, _, 0x4) => self.add_xy(x, y),
+                _ => todo!("opcode {:04x}", opcode),
+            }
         }
-        // }
     }
 
     fn add_xy(&mut self, x: u8, y: u8) {
-        self.registers[x as usize] += self.registers[y as usize];
+        let arg1 = self.registers[x as usize];
+        let arg2 = self.registers[y as usize];
+
+        let (val, overflow) = arg1.overflowing_add(arg2);
+        self.registers[x as usize] = val;
+
+        if overflow {
+            self.registers[0xF] = 1;
+        } else {
+            self.registers[0xF] = 0;
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,24 @@
 use system_programming::CPU;
 
 fn main() {
-    let mut cpu = CPU {
-        current_operation: 0,
-        registers: [0; 2],
-    };
+    let mut cpu = CPU::new();
 
-    cpu.current_operation = 0x8014;
     cpu.registers[0] = 5;
     cpu.registers[1] = 10;
+    cpu.registers[2] = 10;
+    cpu.registers[3] = 10;
+
+    let mem = &mut cpu.memory;
+    mem[0] = 0x80;
+    mem[1] = 0x14;
+    mem[2] = 0x80;
+    mem[3] = 0x24;
+    mem[4] = 0x80;
+    mem[5] = 0x34;
 
     cpu.run();
+
+    assert_eq!(cpu.registers[0], 35);
+
+    println!("5 + 10 + 10 + 10 = {}", cpu.registers[0]);
 }
