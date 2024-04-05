9c58beae5eb06440a221629ddb83592eed82d2f9
Oghenemarho Orukele <orukele.dev@gmail.com>
2024-04-05 20:55:03 +0200

    virtual memory introduction

--- src/lib.rs
+++ src/lib.rs
@@ -2,9 +2,11 @@ pub mod chip8;
 pub mod particles_simulator;
 pub mod q7_format;
 pub mod rand_example;
+pub mod tcp_server;
 
 // public re-exports
 pub use chip8::*;
 pub use particles_simulator::*;
 pub use q7_format::*;
 pub use rand_example::*;
+pub use tcp_server::*;
---
+++ src/tcp_server/server.rs
@@ -0,0 +1,37 @@
+use std::{
+    io::{Read, Write},
+    net::{TcpListener, TcpStream},
+};
+
+pub fn run_sever(ip_address: &str, port: i8) {
+    // Connects to a TCP server running on localhost:8080
+    let address = format!("{}:{}", ip_address, port);
+    let stream = TcpStream::connect(&address).expect("unable to connect to address");
+    let listener = TcpListener::bind(&address).expect("unable to bind to address");
+
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                handle_client(stream);
+            }
+            Err(e) => {
+                eprintln!("Failed: {}", e);
+            }
+        }
+    }
+}
+
+fn handle_client(mut stream: TcpStream) {
+    let mut buf = [0; 512];
+    loop {
+        let bytes_read = stream.read(&mut buf).expect("Failed to read from client");
+
+        if bytes_read == 0 {
+            return;
+        }
+
+        stream
+            .write_all(&buf[0..bytes_read])
+            .expect("Failed to write to client");
+    }
+}
